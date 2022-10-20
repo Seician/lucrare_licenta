39b8f27fd63288f30cf5aa16d6f1927d55c90ae9
Seician <seician09@yahoo.com>
2022-10-20 23:11:53 +0300

    Adding icon_and_text widget

--- lib/home/food_page_body.dart
+++ lib/home/food_page_body.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_complete_guide/utils/colors.dart';
+import 'package:flutter_complete_guide/widgets/big_text.dart';
+import 'package:flutter_complete_guide/widgets/icon_and_text_widget.dart';
+import 'package:flutter_complete_guide/widgets/small_text.dart';
 
 class FoodPageBody extends StatefulWidget {
   const FoodPageBody({super.key});
@@ -31,7 +34,7 @@ class _FoodPageBodyState extends State<FoodPageBody> {
     return Stack(children: [
       Container(
         height: 220,
-        margin: const EdgeInsets.only(left: 5, right: 5),
+        margin: const EdgeInsets.only(left: 10, right: 10),
         decoration: BoxDecoration(
             borderRadius: BorderRadius.circular(30),
             color: index.isEven ? const Color(0xFF69c5df) : Color(0xFF9294cc),
@@ -42,11 +45,68 @@ class _FoodPageBodyState extends State<FoodPageBody> {
       Align(
         alignment: Alignment.bottomCenter,
         child: Container(
-          height: 150,
-          margin: const EdgeInsets.only(left: 20, right: 20, bottom: 15),
+          height: 120,
+          margin: const EdgeInsets.only(left: 30, right: 30, bottom: 20),
           decoration: BoxDecoration(
             borderRadius: BorderRadius.circular(30),
             color: Colors.white,
+          ),
+          child: Container(
+            padding: const EdgeInsets.only(top: 15, left: 15, right: 15),
+            child:
+                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+              BigText(text: "American Burger"),
+              const SizedBox(
+                height: 10,
+              ),
+              Row(
+                children: [
+                  Wrap(
+                    children: List.generate(
+                        5,
+                        (index) => const Icon(
+                              Icons.star,
+                              color: AppColors.mainColor,
+                              size: 15,
+                            )),
+                  ),
+                  const SizedBox(
+                    width: 10,
+                  ),
+                  SmallText(text: "4.5"),
+                  const SizedBox(
+                    width: 10,
+                  ),
+                  SmallText(text: "1287"),
+                  const SizedBox(
+                    width: 10,
+                  ),
+                  SmallText(text: "comments"),
+                ],
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Row(
+                children: const [
+                  IconAndTextWidget(
+                    icon: Icons.circle_sharp,
+                    text: "Normal",
+                    iconColor: AppColors.iconColor1,
+                  ),
+                  IconAndTextWidget(
+                    icon: Icons.location_on,
+                    text: "1.7km",
+                    iconColor: AppColors.mainColor,
+                  ),
+                  IconAndTextWidget(
+                    icon: Icons.access_time_rounded,
+                    text: "32min",
+                    iconColor: AppColors.iconColor2,
+                  )
+                ],
+              )
+            ]),
           ),
         ),
       ),
