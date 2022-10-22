758d7521e84045dccaf05117b384e0b90d30e73f
Seician <seician09@yahoo.com>
2022-10-22 21:16:29 +0300

    Adding product models

--- lib/controllers/popular_product_controller.dart
+++ lib/controllers/popular_product_controller.dart
@@ -1,6 +1,8 @@
 import 'package:flutter_complete_guide/data/repository/popular_product_repo.dart';
 import 'package:get/get.dart';
 
+import '../models/products_model.dart';
+
 class PopularProductController extends GetxController {
   final PopularProductRepo popularProductRepo;
   List<dynamic> _popularProductList = [];
@@ -14,7 +16,7 @@ class PopularProductController extends GetxController {
     if (response.statusCode == 200) {
       // the response is a .json type data
       _popularProductList = [];
-   //   _popularProductList.addAll();
+      _popularProductList.addAll(Product.fromJson(response.body).products);
       update(); // similar with set state;
     } else {}
   }
