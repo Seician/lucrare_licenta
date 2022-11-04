615bd3942e58809cd5692ed54a0cf4061e51b14e
Seician <seician09@yahoo.com>
2022-11-04 21:35:34 +0200

    Adding persistent navbar plugin

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_complete_guide/pages/home/main_food_page.dart';
 import 'package:flutter_complete_guide/utils/colors.dart';
+// import 'package:persistent_bottom_nav_bar/persistent_tab_view.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -12,6 +13,7 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   int _selectedIndex = 0;
+  // late PersistentTabController _controller;
   List pages = [
     MainFoodPage(),
     Container(child: Center(child: Text("Next page"))),
@@ -25,6 +27,50 @@ class _HomePageState extends State<HomePage> {
     });
   }
 
+  // @override
+  // void initState() {
+  //   // TODO: implement initState
+  //   super.initState();
+  //   _controller = PersistentTabController(initialIndex: 0);
+  // }
+
+  // List<Widget> _buildScreens() {
+  //   return [
+  //     MainFoodPage(),
+  //     Container(child: Center(child: Text("Next page"))),
+  //     Container(child: Center(child: Text("Next Next page"))),
+  //     Container(child: Center(child: Text("Next Next Next page"))),
+  //   ];
+  // }
+
+  // List<PersistentBottomNavBarItem> _navBarsItems() {
+  //   return [
+  //     PersistentBottomNavBarItem(
+  //       icon: const Icon(CupertinoIcons.home),
+  //       title: ("Home"),
+  //       activeColorPrimary: CupertinoColors.activeBlue,
+  //       inactiveColorPrimary: CupertinoColors.systemGrey,
+  //     ),
+  //     PersistentBottomNavBarItem(
+  //       icon: const Icon(CupertinoIcons.archivebox_fill),
+  //       title: ("Archive"),
+  //       activeColorPrimary: CupertinoColors.activeBlue,
+  //       inactiveColorPrimary: CupertinoColors.systemGrey,
+  //     ),
+  //      PersistentBottomNavBarItem(
+  //       icon: const Icon(CupertinoIcons.cart_fill),
+  //       title: ("Cart"),
+  //       activeColorPrimary: CupertinoColors.activeBlue,
+  //       inactiveColorPrimary: CupertinoColors.systemGrey,
+  //     ),
+  //     PersistentBottomNavBarItem(
+  //       icon: const Icon(CupertinoIcons.person),
+  //       title: ("Me"),
+  //       activeColorPrimary: CupertinoColors.activeBlue,
+  //       inactiveColorPrimary: CupertinoColors.systemGrey,
+  //     ),
+  //   ];
+  // }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -58,4 +104,43 @@ class _HomePageState extends State<HomePage> {
           ]),
     );
   }
+ 
+ // Persistent NavBar
+
+  // @override
+  // Widget build(BuildContext context) {
+  //   return PersistentTabView(
+  //     context,
+  //     controller: _controller,
+  //     screens: _buildScreens(),
+  //     items: _navBarsItems(),
+  //     confineInSafeArea: true,
+  //     backgroundColor: Colors.white, // Default is Colors.white.
+  //     handleAndroidBackButtonPress: true, // Default is true.
+  //     resizeToAvoidBottomInset:
+  //         true, // This needs to be true if you want to move up the screen when keyboard appears. Default is true.
+  //     stateManagement: true, // Default is true.
+  //     hideNavigationBarWhenKeyboardShows:
+  //         true, // Recommended to set 'resizeToAvoidBottomInset' as true while using this argument. Default is true.
+  //     decoration: NavBarDecoration(
+  //       borderRadius: BorderRadius.circular(10.0),
+  //       colorBehindNavBar: Colors.white,
+  //     ),
+  //     popAllScreensOnTapOfSelectedTab: true,
+  //     popActionScreens: PopActionScreensType.all,
+  //     itemAnimationProperties: ItemAnimationProperties(
+  //       // Navigation Bar's items animation properties.
+  //       duration: Duration(milliseconds: 200),
+  //       curve: Curves.ease,
+  //     ),
+  //     screenTransitionAnimation: ScreenTransitionAnimation(
+  //       // Screen transition animation on change of selected tab.
+  //       animateTabTransition: true,
+  //       curve: Curves.ease,
+  //       duration: Duration(milliseconds: 200),
+  //     ),
+  //     navBarStyle:
+  //         NavBarStyle.style5, // Choose the nav bar style with this property.
+  //   );
+  // }
 }
